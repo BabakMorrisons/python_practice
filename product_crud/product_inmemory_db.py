d8b7cc4e8f8e22c1d87cec3969f0179a0c47d14e
BabakMorrisons <102218100+BabakMorrisons@users.noreply.github.com>
2023-01-12 16:28:25 +0000

    update

--- product_crud/product_inmemory_db.py
+++ product_crud/product_inmemory_db.py
@@ -1,22 +1,24 @@
 class ProductInMemoryDb:
     ''' Product In Memory Class'''
-    product_list=[dict]
+    product_list:list=[]
 
     def __init__(self,data:dict):
         self.data = data
         
     def add(data:dict=None) -> int:
-        if data is None:
+         if data is None:
             data = self.data
-            
-        product_list.append(data)
+        self.product_listdb.append(data)
         return 1
     
-    def update(data:dict | None) -> int:
-        for item in cls.product_list:
+    def update(self,data:dict=None) -> int:
+        for item in self.product_list:
             if item['id']==id:
+                if data is None:
+                    data = self.data
                 item.update(data)
                 return 1
+        return 0
 
     def find_by_id(id:int) -> dict:
         next(item for item in product_list if item['id']==id)  
